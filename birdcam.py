80d10543fae0c5eb773296ffc7c9e3aeef7649af
DonCanalie <doncanalie2@web.de>
2015-11-05 17:30:47 +0100

    Buttons for LED1 added

--- birdcam.py
+++ birdcam.py
@@ -10,7 +10,7 @@ GPIO3 = 15
 GPIO4 = 16
 GPIO5 = 18
 
-GPIO.setmode(GPIO.BOARD) ## setting GPIO pin numbering to Board format
+GPIO.setmode(GPIO.BOARD) ## setting GPIO pin numbering to Board format --> Use Pin-Number, not GPIO number
 GPIO.setup(GPIO0, GPIO.OUT) ## Setting GPIO 0 on Pin 11 to Output mode
 #Defining the index page
 urls = ('/', 'index')
@@ -22,6 +22,8 @@ app = web.application(urls, globals())
 my_form = form.Form(
  form.Button("btn", id="btnR", value="btnLed0On", html="LED0 on", class_="on"),
  form.Button("btn", id="btnG", value="btnLed0Off", html="LED0 off", class_="off"),
+ form.Button("btn", id="btnR", value="btnLed1On", html="LED1 on", class_="on"),
+ form.Button("btn", id="btnG", value="btnLed1Off", html="LED1 off", class_="off"),
  
 )
 
@@ -38,10 +40,17 @@ class index:
         userData = web.input()
         if userData.btn == "btnLed0On":
             GPIO.output(GPIO0,True) #Turn on the LED
-            print "LED is ON"   #prints the status in Pi's Terminal
+            print "LED0 is ON"   #prints the status in Pi's Terminal
         elif userData.btn == "btnLed0Off":
             GPIO.output(GPIO0,False) #Turn of the LED
-            print "LED is OFF" #prints the status in Pi's Terminal
+            print "LED0 is OFF" #prints the status in Pi's Terminal
+        elif userData.btn == "btnLed1On":
+            GPIO.output(GPIO1,False) #Turn of the LED
+            print "LED1 is OFF" #prints the status in Pi's Termina        
+        elif userData.btn == "btnLed1Off":
+            GPIO.output(GPIO1,False) #Turn of the LED
+            print "LED1 is OFF" #prints the status in Pi's Terminal
+            
         raise web.seeother('/')
 # run
 if __name__ == '__main__':
