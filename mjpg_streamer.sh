1cbc5d66c4f0db3386d62569a2923380322f0288
DonCanalie <doncanalie2@web.de>
2015-11-08 00:40:25 +0000

    mjpg_streamer.sh updated

--- mjpg_streamer.sh
+++ mjpg_streamer.sh
@@ -5,9 +5,9 @@
 # USB-Webcam. The script needs to be run with root access.
 
 PORT=8080
-FPS=5
+FPS=10
 WIDTH=640
-HEIGHT=360
+HEIGHT=400
 WWW=/usr/local/www
 
 export LD_LIBRARY_PATH=/usr/local/lib
