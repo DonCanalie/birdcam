f51d7dc17905c0b9d122b6365c32b07f2d83dfb9
DonCanalie <doncanalie2@web.de>
2015-11-14 03:43:58 +0000

    * several new features added

--- birdcam.py
+++ birdcam.py
@@ -3,7 +3,10 @@
  Moreover, there is the possibillity to control IR-Light-LED's to light the view. """
 import web
 import os
+import sqlite3
 #import RPi.GPIO as GPIO
+# https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/tree/master/Adafruit_DHT_Driver_Python
+#import dhtreader
 
 from web import form
 
@@ -20,20 +23,32 @@ GPIO5 = 18
 WEBCAM1 = "1"
 WEBCAM2 = "2"
 
+DHT_TYPE = 22
+DHT_SOURCE = GPIO4
+
+DATABASE = "birdcam.db"
+
+# initialize DHT22-Reader
+#dhtreader.init()
+
 # GPIO.BOARD --> Use Pin-Number, not GPIO number
 #GPIO.setmode(GPIO.BOARD) ## setting GPIO pin numbering to Board format
 #GPIO.setup(GPIO0, GPIO.OUT) ## Setting GPIO 0 on Pin 11 to Output mode
 #GPIO.setup(GPIO1, GPIO.OUT) ## Setting GPIO 1 on Pin 12 to Output mode
 
-#Defining the index page
-URLS = ('/', 'Index')
+#Defining the urls
+URLS = (
+    "/", "Index", 
+    "/climate", "Climate"
+)
+
 RENDER = web.template.render('templates') #index.html is stored in '/templates' folder
 APP = web.application(URLS, globals())
 
 """ Defining the buttons. 'id' stands for HTML id of the element.
     'value' is the value of the button as perceived by Python.
     'html' is the text displayed in HTML page. 'class_' is HTML class"""
-CONTROLLER = form.Form(
+RIGHT = form.Form(
     form.Button("btn", id="btnR1", value="btnLed0On", html="LED0 on", class_="on"),
     form.Button("btn", id="btnG1", value="btnLed0Off", html="LED0 off", class_="off"),
     form.Button("btn", id="btnR2", value="btnLed1On", html="LED1 on", class_="on"),
@@ -42,9 +57,59 @@ CONTROLLER = form.Form(
     form.Button("btn", id="btnY2", value="btnWebcam2", html="Webcam 2", class_="cam"),
 )
 
-TOPLEFT = form.Form(
+LEFT = form.Form(
+    form.Textbox("txt1", web.form.notnull, description="Temperature:", style="float: lefty; width: 50px;"),
+    form.Textbox("txt2", web.form.notnull, description="Humidity:", style="float: left; width: 50px;"),
+    form.Textbox("txt3", web.form.notnull, description="recorded at ", style="float: left; width: 50px;"),
+    form.Button("btn", id="btnG3", value="btnRefreshClimate", html="Refresh", class_="off", style="float: left;"),
+    form.Button("btn", id="btnY3", value="btnTimeline", html="Timeline", class_="cam", style="float: left;")
+)
+
+TOPRIGHT = form.Form(
     form.Button("btn", id="btnR3", value="btnReboot", html="Reboot", class_="off"),
 )
+
+def getDBCursor():
+    global db
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    return cursor
+
+def getClimateData(limit):
+    """ TODO: Evtl. auf dht22 umbauen fuer ganz aktuellen zugriff. Haengt jedoch davon ab, wie schnell der Sensor anwortet. 
+        Sonst besser wie gehabt aus der DB lesen """
+            
+    cursor = getDBCursor()
+    commandtext = "SELECT recorded, temperature, humidity FROM climate " \
+        "ORDER BY recorded DESC"
+        
+    if limit:           
+        cursor.execute(commandtext + " LIMIT (?)", (limit))
+    else:
+        cursor.execute(commandtext)
+    
+    result = cursor.fetchall()
+    #print result
+    db.close()
+    return result
+
+def setClimateData():
+    climate_cur = getClimateData("1")
+    if len(climate_cur) > 0:
+        #print len(climate_cur)
+        row = climate_cur[0]
+        #print "climate recorded at {0} - temperature: {1}, humidity: {2}".format(
+        #    row[0], row[1], row[2])    
+        left.txt1.value = row[1]
+        left.get('txt2').value = row[2]
+        left.txt3.value = row[0]
+    return
+
+class Climate(object):
+    
+    def GET(self):
+        # Simply use this https://github.com/Pyplate/rpi_temp_logger
+        return "TEST"
 
 class Index(object):
     """ define the task of index page """
@@ -55,12 +120,18 @@ class Index(object):
     def GET(self):
         """ rendering the HTML page """
         global webcam
-        global form1
-        global form2
-        form1 = CONTROLLER()
-        form2 = TOPLEFT()
-        webcam = WEBCAM1
-        return RENDER.index(form1, form2, "Raspberry Pi LED Blink", webcam)
+        global topright
+        global left
+        global right
+        
+        right = RIGHT()
+        left = LEFT()
+        topright = TOPRIGHT()        
+        webcam = WEBCAM1   
+        
+        setClimateData()
+                
+        return RENDER.index(right, left, topright, "Raspberry Pi LED Blink", webcam)
 
     def POST(self):
         """ posting the data from the webpage to Pi """
@@ -82,16 +153,20 @@ class Index(object):
             print "LED1 is OFF" #prints the status in Pi's Terminal
         elif userdata.btn == "btnReboot":
             print "System is going to for reboot!" #prints the status in Pi's Terminal 
-            os.system("sudo reboot")           
+            os.system("sudo reboot")   
+        elif userdata.btn =="btnRefreshClimate":
+            setClimateData() # wird beim refresh eigentlich gemacht. alternativ: nur initial und sonst explizit ausfuehren
         elif userdata.btn == "btnWebcam1":
             print "Switched to Webcam 1" #prints the status in Pi's Terminal
             webcam = WEBCAM1
         elif userdata.btn == "btnWebcam2":
             print "Switched to Webcam 2" #prints the status in Pi's Terminal
-            webcam = WEBCAM2
-
-        #raise web.seeother('/')
-        return RENDER.index(form1, form2, "Raspberry Pi LED Blink", webcam)
+            webcam = WEBCAM2     
+        elif userdata.btn == "btnTimeline":
+            raise web.seeother('/climate')
+        
+        #raise web.seeother('/') # Geht hier nicht, da der Parameter 'webcam' sich geaendert hat
+        return RENDER.index(right, left, topright, "Raspberry Pi LED Blink", webcam)
 # run
 if __name__ == '__main__':
     web.httpserver.runsimple(APP.wsgifunc(), (SOCKET_IP, SOCKET_PORT))
