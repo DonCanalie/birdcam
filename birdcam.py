2622d2aee45fbd1294b1df1555b61f06f1a4994d
DonCanalie <doncanalie2@web.de>
2015-11-14 17:46:25 +0000

    * climate.py added --> needs to be fixed

--- birdcam.py
+++ birdcam.py
@@ -7,6 +7,8 @@ import sqlite3
 #import RPi.GPIO as GPIO
 # https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code/tree/master/Adafruit_DHT_Driver_Python
 #import dhtreader
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 from web import form
 
@@ -109,7 +111,19 @@ class Climate(object):
     
     def GET(self):
         # Simply use this https://github.com/Pyplate/rpi_temp_logger
-        return "TEST"
+        # https://plot.ly/python/time-series/
+        
+
+        data = [
+            go.Scatter(
+                x=['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+                y=[1, 3, 6]
+            )
+        ]
+        
+        plot_url = py.plot(data, filename='date-axes')
+        
+        return plot_url
 
 class Index(object):
     """ define the task of index page """
