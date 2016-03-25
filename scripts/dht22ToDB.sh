49aae68fae731e01d5de011849c7587ac42af12c
DonCanalie <doncanalie2@web.de>
2016-03-25 01:24:58 +0000

    dht22ToDB.sh fixed

--- scripts/dht22ToDB.sh
+++ scripts/dht22ToDB.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sqlite3 ../birdcam.db "insert into climate values(`python ../readDHT22.py`);"
+DIR=$(dirname $0)
+sqlite3 $DIR/../birdcam.db "insert into climate values(`python $DIR/../readDHT22.py`);"
